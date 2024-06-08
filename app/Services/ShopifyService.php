3f83f630fb6c3fcef8d4bcd2e12e3461bd45484f
rosmis <rosmis123@gmail.com>
2024-06-08 13:49:30 +0200

    added first fetchProducts endpoint logic

--- app/Services/ShopifyService.php
+++ app/Services/ShopifyService.php
@@ -7,6 +7,7 @@ namespace App\Services;
 use App\Contracts\ShopifyInterface;
 use App\Dto\ShopifyConfigDto;
 use Illuminate\Support\Collection;
+use Shopify\Clients\Storefront;
 use Shopify\Context;
 
 readonly class ShopifyService implements ShopifyInterface
@@ -19,6 +20,27 @@ readonly class ShopifyService implements ShopifyInterface
 
     public function fetchProduts(): Collection
     {
-        dd('fetchProduts');
+        $storefrontClient = new Storefront(
+            $this->config->hostName,
+            $this->config->apiKey
+        );
+        // Use `query` method and pass your query as `data`
+        $queryString = <<<'QUERY'
+            {
+                products (first: 10) {
+                    edges {
+                        node {
+                            id
+                            title
+                            descriptionHtml
+                        }
+                    }
+                }
+            }
+        QUERY;
+
+        $response = $storefrontClient->query(data: $queryString);
+
+        dd($response->getDecodedBody());
     }
 }
