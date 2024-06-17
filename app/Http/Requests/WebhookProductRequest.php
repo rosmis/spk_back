f323a779427a336e788a017fa89ee2e1e0669e60
rosmis <rosmis123@gmail.com>
2024-06-17 15:55:57 +0200

    added webhook product validation request

---
+++ app/Http/Requests/WebhookProductRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WebhookProductRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'admin_graphql_api_id' => 'required|string',
+            'handle' => 'required|string',
+            'title' => 'required|string',
+            'body_html' => 'required|string',
+
+            'variants.*.admin_graphql_api_id' => 'required|string',
+            'variants.*.title' => 'required|string',
+            'variants.*.price' => 'required|numeric',
+            'variants.*.inventory_quantity' => 'required|integer',
+            'variants.*.id' => 'required|integer',
+
+            'images.*.admin_graphql_api_id' => 'required|string',
+            'images.*.src' => 'required|string',
+            'images.*.alt' => 'nullable|string',
+            'images.*.id' => 'required|integer',
+        ];
+    }
+}
