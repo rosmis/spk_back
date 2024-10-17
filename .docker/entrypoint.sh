b15162ba687fb5ef035935a49848cb53fb20e92b
rosmis <rosmis123@gmail.com>
2024-10-17 17:08:55 +0200

    trying shit

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Wait for database to be ready
-until php artisan db:monitor; do
-  echo "Waiting for database connection..."
-  sleep 1
-done
+# # Wait for database to be ready
+# until php artisan db:monitor; do
+#   echo "Waiting for database connection..."
+#   sleep 1
+# done
 
 # Run migrations
 php artisan migrate --force
