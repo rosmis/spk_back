d2f838ddf0a69fa02b090e9cbc01835cd2bd1a9c
rosmis <rosmis123@gmail.com>
2024-10-18 16:06:11 +0200

    update on dockerfile prod

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # # Wait for database to be ready
-# until php artisan db:monitor; do
-#   echo "Waiting for database connection..."
-#   sleep 1
-# done
+until php artisan db:monitor; do
+  echo "Waiting for database connection..."
+  sleep 1
+done
 
 # Run migrations
 php artisan migrate --force
