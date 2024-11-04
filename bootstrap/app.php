13f80a1383d650ac27516351c820b10ad7fc7e33
rosmis <rosmis123@gmail.com>
2024-11-04 19:54:29 +0100

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,5 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     'message' => $e->getMessage(),
                 ], $e->getCode());
             }
+
+            return $e;
         });
     })->create();
