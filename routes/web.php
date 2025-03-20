df8916b3c2b100dd8dd5ce2b47dd54cd6becdde5
rosmis <rosmis123@gmail.com>
2025-03-20 20:41:38 +0100

    commented out register route

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login'])
     ->middleware('throttle:3,1');
-Route::post('register', [AuthController::class, 'register'])
-    ->middleware('throttle:3,1');
 Route::post('logout', [AuthController::class, 'logout'])
     ->middleware('throttle:3,1');
 
